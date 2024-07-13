0a67ae5d1e21c11fa8445894e9027ccc378a7ef4
LVCarnevalli <LVCarnevalli@users.noreply.github.com>
2024-07-13 00:15:12 -0300

    config: dns

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/logo-dark.svg",
 
   // Set the production url of your site here
-  url: "https://lvcarnevalli.github.io",
+  url: "https://umbandaponto.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/ponto/",
